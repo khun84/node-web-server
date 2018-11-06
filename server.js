859ac482bfcaa965b078454a887da874b58e586b
khun84 <khun84@gmail.com>
2018-11-06 18:05:06 +0800

    45 Express middleware

--- server.js
+++ server.js
@@ -1,10 +1,28 @@
 const express = require('express');
 const hbs = require('hbs');
+const fs = require('fs');
 
 let app = express();
 
 hbs.registerPartials(__dirname + '/views/partials');
 app.set('view engine', 'hbs');
+
+// Middleware configuration block
+app.use((req, res, next) => {
+  let now = new Date().toString();
+  let log = `${now}: ${req.method} ${req.url}`;
+  console.log(`${now}: ${req.method} ${req.url}`);
+  fs.appendFile('server.log', log + '\n', (err) => {
+    if (err) {
+      console.log('Unable to append to server.log');
+    }
+  });
+
+  next(); // have to call this so the req will be forwarded to the next middleware, otherwise req stucked here
+});
+app.use((req, res, next) => {
+  res.render('maintenance.hbs');
+});
 app.use(express.static(__dirname + '/public'));
 
 hbs.registerHelper('getCurrentYear', () => {
