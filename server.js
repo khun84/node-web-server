3b892859c9d6e7b1da8d1e6eb716491306a2312c
khun84 <khun84@gmail.com>
2018-11-06 17:27:19 +0800

    43 Rendering templates with data

--- server.js
+++ server.js
@@ -1,21 +1,24 @@
 const express = require('express');
+const hbs = require('hbs');
 
 let app = express();
 
+app.set('view engine', 'hbs');
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', (req, res) => {
-  res.send({
-    name: 'Daniel',
-    likes: [
-      'Biking',
-      'Cities'
-    ]
+  res.render('home.hbs', {
+    pageTitle: 'Home Page',
+    currentYear: new Date().getFullYear(),
+    welcomeMessage: 'Welcome to my website'
   });
 });
 
 app.get('/about', (req, res) => {
-  res.send('About Page');
+  res.render('about.hbs', {
+    pageTitle: 'About Page',
+    currentYear: new Date().getFullYear()
+  });
 });
 
 app.get('/bad', (req, res) => {
